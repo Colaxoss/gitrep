dbc2320971491374f84310b7a9563b91b9bf0356
Colaxoss <119417305+Colaxoss@users.noreply.github.com>
2023-01-17 14:16:43 +0100

    Taschenrechner

--- Aufg.02/Aufg.02.03/main.c
+++ Aufg.02/Aufg.02.03/main.c
@@ -5,14 +5,14 @@ int main ()
     double EZ, ZZ;
   
   
-    printf("Willkommen beim Taschenrechner zum addieren!\n\n");
+    printf("Willkommen beim Taschenrechner zum Addieren!\n\n");
   
     printf("Bitte geben Sie die erste Zahl ein:\n");
     scanf("%le", &EZ);
     printf("Bitte geben Sie die zweite Zahl ein:\n");
     scanf("%le", &ZZ);
   
-    printf("Das Ergebnis lautet:\n", EZ+ZZ);
+    printf("Die Summe ergibt %.2le\n\n", zahl1 + zahl2);
   
   
     return 0;
