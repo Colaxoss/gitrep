a3e7de03aa911c5ba2d5aedcbd4b1c91f595d585
Colaxoss <119417305+Colaxoss@users.noreply.github.com>
2023-01-17 14:08:13 +0100

    Update main.c

--- Aufg.02/Aufg.02.03/main.c
+++ Aufg.02/Aufg.02.03/main.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+int main ()
+{
+    double EZ, ZZ;
+  
+  
+    printf("Willkommen beim Taschenrechner zum addieren!\n\n");
+  
+    printf("Bitte geben Sie die erste Zahl ein:\n");
+    scanf("%d", &EZ);
+  
+    printf("Bitte geben Sie die zweite Zahl ein:\n");
+    scanf("%d", &ZZ);
+  
+    printf("Das Ergebnis lautet:", EZ+ZZ);
+  
+  
+    return 0;
+}
