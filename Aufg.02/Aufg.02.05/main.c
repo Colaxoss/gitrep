2f526a85845a0e958d424ed12d9bd48c8e89b4ed
Colaxoss <119417305+Colaxoss@users.noreply.github.com>
2023-01-17 15:06:08 +0100

    Verbrauchsrechner

--- Aufg.02/Aufg.02.05/main.c
+++ Aufg.02/Aufg.02.05/main.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+
+int main ()
+{
+    float DV, GefahreneKilometer;
+    
+    printf("\nWilkommen beim Verbrauchsrechner für Ihre Fahrt!\n\n");
+  
+    printf("Was verbraucht ihr KFZ in 100km?\n");
+    scanf("%f", &DV);
+    printf("Wie viele Kilometer sind Sie gefahren?");
+    scanf("%f", &GefahreneKilometer);
+  
+    printf("Sie haben %f Kilometer verbraucht.", DV/100*GefahreneKilometer);
+    
+    return 0;
+    
+}
