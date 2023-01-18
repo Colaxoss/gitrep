0fec4c4abafaba3018c0e4450b36c0c69527b138
Colaxoss <119417305+Colaxoss@users.noreply.github.com>
2023-01-18 18:15:03 +0100

    Kreisrechner

--- Aufg.02/Aufg.02.06/main.c
+++ Aufg.02/Aufg.02.06/main.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#define PI 3.14
+
+int main()
+{
+    float r;
+  
+    printf("\nWillkommen beim Flächeninhaltsrechner für Kreise!\n\n");
+  
+    printf("Bitte geben Sie den Radius des Kreises ein:\n");
+    scanf("%f", &r);
+    
+    printf("Der Flächeninhalt beträgt: %f", PI*r*r);
+  
+    return 0;
+}
