35a6e997d68bf3c883914467d97f3db204508dcf
Colaxoss <119417305+Colaxoss@users.noreply.github.com>
2023-01-19 10:17:26 +0100

    Update main.c

--- Aufg.02/Aufg.02.08/main.c
+++ Aufg.02/Aufg.02.08/main.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+
+int main()
+{
+    int AS;
+  
+    printf("\nWillkommen beim Tagesrechner!\n\n");
+    
+    printf("Bitte geben Sie die Anzahl der Tage an.\n");
+    scanf("%d", &AS);
+           
+    printf("\n%d ", AS);
+    AS = AS/60/60 % 24;
+    printf(" Sekunden sind %d Tage\n\n", AS);
+    //printf("Es bleiben Minuten\n\n", AT*24*60);
+    //printf("Tage haben %d Sekunden\n\n", AT*24*60*60);
+           
+    return 0; 
+}
