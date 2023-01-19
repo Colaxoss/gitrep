06a4347873e160fc174323e63c1438d925c7a35e
Colaxoss <119417305+Colaxoss@users.noreply.github.com>
2023-01-19 12:24:17 +0100

    Update main.c

--- Aufg.02/Aufg.02.08/main.c
+++ Aufg.02/Aufg.02.08/main.c
@@ -14,7 +14,7 @@ int main()
     printf("\n%d ", AS);
     cache = AS/60/60/24;
     printf(" Sekunden sind %d Tage\n\n", cache);
-    printf("Es bleiben %d Stunden\n\n", AS/60 % 60);
+    printf("Es bleiben %d Stunden\n\n", (AS/60) % 60);
     printf("Es bleiben %d Minuten\n\n", AS % 60);
     Sek = AS-(cache*24*60*60);
     printf("Es bleiben %d Sekunden\n\n", Sek);
