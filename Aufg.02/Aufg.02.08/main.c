f90dfe97c0a6222e3111b12af2287688199e110a
Colaxoss <119417305+Colaxoss@users.noreply.github.com>
2023-01-19 14:42:05 +0100

    Update main.c

--- Aufg.02/Aufg.02.08/main.c
+++ Aufg.02/Aufg.02.08/main.c
@@ -9,18 +9,16 @@ int main()
         printf("Bitte geben Sie die Anzahl der Sekunden an.\n");
         scanf("%d", &AS);
     
-        Tag = ((AS/60)/60)/24;
-        Tag -= AS*24*60*60;
-        Std = AS/60/60;
-        Std -= AS*60*60;
-        Min = AS/60;
-        Min -= AS*60;
-        Sek = Min*60;
-    
         printf("\n%d ", AS);
         printf(" Sekunden sind %d Tage\n\n", Tag);
+        Tag -= AS*24*60*60;
+        Std = AS/60/60;
         printf("Es bleiben %d Stunden\n\n", Std);
+        Std -= AS*60*60;
+        Min = AS/60;
         printf("Es bleiben %d Minuten\n\n", Min);
+        Min -= AS*60;
+        Sek = Min*60;
         printf("Es bleiben %d Sekunden\n\n", Sek);
            
         return 0; 
