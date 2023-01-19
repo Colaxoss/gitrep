d6f024cf8ad72698742890bb68670de5a235bb05
Colaxoss <119417305+Colaxoss@users.noreply.github.com>
2023-01-19 12:46:43 +0100

    Update main.c

--- Aufg.02/Aufg.02.08/main.c
+++ Aufg.02/Aufg.02.08/main.c
@@ -2,22 +2,23 @@
 
 int main()
 {
-    int AS, Tag, Sek, Std, Min;
+        int AS, Tag, Sek, Std, Min;
   
-    printf("\nWillkommen beim Tagesrechner Reverse!\n\n");
+        printf("\nWillkommen beim Tagesrechner Reverse!\n\n");
     
-    printf("Bitte geben Sie die Anzahl der Sekunden an.\n");
-    scanf("%d", &AS);
+        printf("Bitte geben Sie die Anzahl der Sekunden an.\n");
+        scanf("%d", &AS);
+    
+        Sek = Min*60;
+        Min = AS % 60;
+        Std = (AS/60) % 60;
+        Tag = AS/60/60/24;
+    
+        printf("\n%d ", AS);
+        printf(" Sekunden sind %d Tage\n\n", Tag);
+        printf("Es bleiben %d Stunden\n\n", Std);
+        printf("Es bleiben %d Minuten\n\n", Min);
+        printf("Es bleiben %d Sekunden\n\n", Sek);
            
-    printf("\n%d ", AS);
-    Tag = (AS/60/60/24) % 0;
-    printf(" Sekunden sind %d Tage\n\n", Tag);
-    Std = (AS/60/60) % 0;
-    printf("Es bleiben %d Stunden\n\n", Std);
-    Min = (AS/60) % 0;
-    printf("Es bleiben %d Minuten\n\n", Min);
-    Sek = Min*60;
-    printf("Es bleiben %d Sekunden\n\n", Sek);
-           
-    return 0; 
+        return 0; 
 }
