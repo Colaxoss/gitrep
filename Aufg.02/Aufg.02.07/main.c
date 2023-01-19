0a1f2509e420f6a7acae03a4d14332fc3f80198a
Colaxoss <119417305+Colaxoss@users.noreply.github.com>
2023-01-19 09:18:27 +0100

    test

--- Aufg.02/Aufg.02.07/main.c
+++ Aufg.02/Aufg.02.07/main.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+int main()
+{
+    float AT;
+  
+    printf("\nWillkommen beim Tagesrechner!\n\n);
+    
+    printf("Bitte geben Sie die Anzahl der Tage an.\n");
+    scanf("%f", &AT);
+           
+    printf("\n",Std,"Tage haben %f Stunden", AT);
+           
+    
+    return 0; 
+}
