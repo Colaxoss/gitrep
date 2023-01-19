e41f78ce383ec6d27fe616d58669c411aaf83762
Colaxoss <119417305+Colaxoss@users.noreply.github.com>
2023-01-19 09:21:25 +0100

    Update main.c

--- Aufg.02/Aufg.02.07/main.c
+++ Aufg.02/Aufg.02.07/main.c
@@ -9,7 +9,8 @@ int main()
     printf("Bitte geben Sie die Anzahl der Tage an.\n");
     scanf("%f", &AT);
            
-    printf("\n",AT,"Tage haben %f Stunden", AT);
+    printf("\n");
+    printf(AT,"Tage haben %f Stunden", AT);
            
     
     return 0; 
