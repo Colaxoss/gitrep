12483920b002e186cbab66ab5b9fd80071f685c0
Colaxoss <119417305+Colaxoss@users.noreply.github.com>
2023-01-21 12:41:03 +0100

    größerkleiner

--- Aufg.03/Aufg.03.02/main.c
+++ Aufg.03/Aufg.03.02/main.c
@@ -1,1 +1,26 @@
-#include <stdio.h> 
+#include <stdio.h>
+
+int main ()
+{
+    int Z1, Z2;
+  
+    printf("\nWillkommen beim größerkleiner-Rechner!\n\n");
+    
+    printf("Bitte geben Sie die erste Zahl ein.\n");
+    scanf("%d", &Z1);
+    printf("Bitte geben Sie die zweite Zahl ein.\n");
+    scanf("%d", &Z2);
+    
+    if(Z1<Z2){
+        printf("\nDie erste Zahl ist kleiner als die zweite Zahl.\n");
+    }
+    else if(Z1>Z2){
+        printf("\nDie erste Zahl ist größer als die zweite Zahl.\n");
+    }
+    else if(Z1==Z2){
+        printf("\nSie haben zwei gleiche Zahlen eingegeben\n");
+    }
+    else{
+        printf("\nFehler! Ungültige Zahl!\n");
+    }
+}
