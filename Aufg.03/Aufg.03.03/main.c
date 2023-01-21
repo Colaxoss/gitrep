ee222e5d847863a02cfe0d9083293f6f66f413cb
Colaxoss <119417305+Colaxoss@users.noreply.github.com>
2023-01-21 13:16:25 +0100

    Prüfen

--- Aufg.03/Aufg.03.03/main.c
+++ Aufg.03/Aufg.03.03/main.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+
+int main ()
+{
+  int wert1, wert2, wert3;
+  
+  printf("\nGeben Sie den ersten Wert ein.\n");
+  scanf("%d", wert1);
+  printf("\nGeben Sie den zweiten Wert ein.\n");
+  scanf("%d", wert2);
+  printf("\nGeben Sie den dritten Wert ein.\n");
+  scanf("%d", wert3);
+  
+    if(wert1<wert2<wert3){
+        printf ("%d", &wert2)
+    }
+}
