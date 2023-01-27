9f889f96ae27067387d4c851928cbd5ae9e93d40
Colaxoss <119417305+Colaxoss@users.noreply.github.com>
2023-01-27 14:58:16 +0100

    Notenrechner

--- Aufg.03/Aufg.03.06/main.c
+++ Aufg.03/Aufg.03.06/main.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+int main ()
+{
+    int Eingabe;
+    
+    printf("\nWillkommen beim Notenrechner!\n\n");
+    
+    printf("Bitte geben Sie Ihre Notenpunktzahl ein.\n");
+    scanf("%d", Eingabe);
+  
+    if(Eingabe<0 / Eingabe>10){
+        if(Eingabe>
+    }
+}
